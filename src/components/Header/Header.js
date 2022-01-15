23cffca813274d77c40c954780c9fa47a2bbc485
praiseopara <cpopara2010@yahoo.com>
2022-01-15 13:04:44 -0600

    created components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <div className='head'>
+                    <h1>P.O</h1>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
