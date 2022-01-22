c537b630cd763b3e23f86e920debdc3b3f8dde5e
praiseopara <cpopara2010@yahoo.com>
2022-01-22 16:07:06 -0600

    created nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,34 @@
+import { MenuOutlined } from '@material-ui/icons'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.css'
 
 const Header = () => {
     return (
         <div className='header'>
             <nav>
                 <div className='head'>
-                    <h1>P.O</h1>
+                    <h1>Praise Opara</h1>
                 </div>
+
+                <ul className='menu'>
+                    <li>
+                        <Link to='#'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='#'>Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to='#'>Contact</Link>
+                    </li>
+                    <li>
+                        <Link to='#'>Resume</Link>
+                    </li>
+
+                    <div className="hamburger-menu">
+                        <MenuOutlined className='menu'/>
+                    </div>
+                </ul>
             </nav>
         </div>
     )
