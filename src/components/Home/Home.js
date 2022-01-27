076ea6bf2aecb36b377147eb0f4a3da1c9a47593
praiseopara <cpopara2010@yahoo.com>
2022-01-27 06:04:17 -0600

    editing home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,16 +9,21 @@ function Home() {
 
             <Header/>
 
-            <div className="home-content">
-                <div>
+            <div className="home-section">
+                <div className='home-details'>
                     <h1>PRAISE OPARA</h1>
                     <p>
-                        I'm a Full Stack Web Developer with a background in Petroleum Engineering with acquired knowledge and skills across multiple industries including the Semiconductor, Oil and Gas and safety. 
+                        I'm a Full Stack Web Developer with a background in Petroleum Engineering with acquired knowledge and skills across multiple industries including Semiconductor.
+                        <br />
+                        <br />
                         Earned a certificate in Full Stack Web Development from the University of Texas, Austin with proficiency in the MERN Stack.
                         
                         Always had a passion for creativity and problem solving and being a full stack web developer brings all that to life. 
                         My effective communication with my goal oriented and critical thinking nature combined with my acquired knowledge and skills, excites me to build better experiences on the web.
                     </p>
+                    <button>
+                        Download My Resume
+                    </button>
                 </div>
 
                 <div className="home-img">
